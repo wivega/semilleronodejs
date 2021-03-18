b558fc444858f7fa0e5cc83c1d32b5cdf68624e1
Wilman Jose Vega Castilla <wivega@personalsoft.com.co>
2021-03-18 09:19:30 -0500

    Actualización lección: Funciones

--- NodeJS/Funciones.js
+++ NodeJS/Funciones.js
@@ -15,7 +15,9 @@
  * Hay tres formas en las que se pueden definir funciones:
  * En ES6 se definen lo que son las funciones flecha (Lambda)
  */
-//Declaración de función
+
+
+//Declaración de función: Existen inmediatamente, pueden llamarse en cualquier punto.
 function factorial(x) {
     if (x <= 1) {
         return 1;
@@ -26,7 +28,16 @@ function factorial(x) {
 
 console.log(factorial(5));
 
-//Expresión de función
+/**
+ * Expresión de función. 
+ * 
+ * Las funciones de expresion solo existen cuando se realiza la expresión. 
+ * No pueden llamarse antes de la definición de la expresión.
+ * Buena practica: Las expresiones de función hacerlas como const
+ * 
+ */
+                     
+//
 const factorial2 = function factural2(x){
     if (x <= 1) {
         return 1;
@@ -34,5 +45,47 @@ const factorial2 = function factural2(x){
     return x * factorial2(x-1);
 }
 
+
 console.log(factorial2(6));
+
+const cuadrado = function(x){
+    return x*x;
+}
+
+
+console.log(cuadrado(12));
+
+//expresión que se autoinvoque
+
+let cuadradoV2 = (function(x){
+    return x*x;
+}(8));
+console.log(cuadradoV2);
+
+//
+
+//
+/**
+ * Funciones flechas o lambdas.
+ * 
+ * son tambien expresiones de función, con la diferencia de que son más compactas.
+ * 
+ */
+
+const suma = (x,y) => {
+    return x+y;
+};
+console.log(suma(2,3));
+
+//La anterior función más compacta
+const sumaV2 = (x,y) => x+y;
+
+//Con un solo parámetro (no requiere el paréntesis)
+const polinomio = x => x*x + 2*x +3;
+console.log(polinomio(5));
+
+//Función lambda sin parámetro.
+const devolverConstante = () => 10;
+console.log(devolverConstante());
+
 
